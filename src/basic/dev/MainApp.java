233fe3f169596cf0ac724e6c73127ed2ba72a8e9
loitino <115127711+loitino@users.noreply.github.com>
2022-12-08 15:39:23 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,48 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Thong tin sinh vien
+
+				String name1 = "Nguyen Van Huy ";
+				int msv = 2260718;
+				String cmnd = "125654486";
+				String classNam = "Tin hoc ung dung";
+				String school = "Cao dang cong nghiep Hue";
+				double diem = 7.0;
+
+						//Thong tin cong nhan
+
+						String name2 = "Nguyen Van Huy";
+				String address = "Thua Thien Hue";
+				double he_so_luong = 2.10;
+				String sdt = "0774489258";
+
+
+				//Thong tin mot chiec xe car
+
+				String nameCar = "moto 2022";
+				 String hang = "Honda";
+				 int gia = 115000000;
+				 String color = "gray";
+					System.out.println("ho va ten " +name1);	
+					System.out.println("mvs " +msv);
+					System.out.println("cmnd " +cmnd);
+					System.out.println("Lop "+ classNam);
+					System.out.println("Truong " + school);
+					System.out.println("dtb "+diem);
+					System.out.println("ten "+ name2);
+					System.out.println("dia chi "+address);
+					System.out.println("sdt "+sdt);
+					System.out.println("he so luong "+he_so_luong);
+					System.out.println("ten "+nameCar);
+					System.out.println("hang "+hang);
+					System.out.println("gia "+gia);
+					System.out.println("mau "+color);
+
+
+	}
+
+}
